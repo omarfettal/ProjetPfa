f03379ad3b858a0b18fcc0ebd6eaeb81e3c54960
omarfettal <121773426+omarfettal@users.noreply.github.com>
2023-05-07 21:58:41 +0200

    Update cars.py

--- Projet pfa/cars.py
+++ Projet pfa/cars.py
@@ -6,15 +6,353 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
+def recherche():
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('RECHERCHE')
+
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    search_query=search_entry.get()
+
+#marque
+    for row in tree.get_children():
+        tree.delete(row)
+
+    query = f"SELECT * FROM voitures WHERE marque LIKE '%{search_query}%'"
+    mycursor.execute(query)
+    resultat1= mycursor.fetchall()
+    
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9])
+
+#type carburant
+    for row in tree.get_children():
+        tree.delete(row)
+
+    query = f"SELECT * FROM voitures WHERE type_carburant LIKE '%{search_query}%'"
+    mycursor.execute(query)
+    resultat1= mycursor.fetchall()
+    
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9])
+
+# nombres de places
+    for row in tree.get_children():
+        tree.delete(row)
+
+    query = f"SELECT * FROM voitures WHERE nombres_de_places LIKE '%{search_query}%'"
+    mycursor.execute(query)
+    resultat1= mycursor.fetchall()
+    
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9])
+
+#transmission
+    for row in tree.get_children():
+        tree.delete(row)
+
+    query = f"SELECT * FROM voitures WHERE transmission LIKE '%{search_query}%'"
+    mycursor.execute(query)
+    resultat1= mycursor.fetchall()
+    
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9])
+
+#prix par jour
+    for row in tree.get_children():
+        tree.delete(row)
+
+    query = f"SELECT * FROM voitures WHERE prix_par_jour='{search_query}'"
+    mycursor.execute(query)
+    resultat1= mycursor.fetchall()
+    
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9])        
 
 
 
-conn= pymysql.connect(host="localhost", user="root", password="omarfettal10", database="cars")
-cursor =conn.cursor()
 
 
 
-def show_voitures():
+
+
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()            
+
+
+
+
+
+def show_dacia():
+  
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('DACIA')
+
+    # treeview
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='dacia'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()
+
+
+def show_renault():
+  
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('RENAULT')
+
+    # treeview
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='renault'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()
+
+
+
+def show_ford():
+  
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('FORD')
+
+    # treeview
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='ford'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()
+
+
+def show_landrover():
+  
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('LAND ROVER')
+
+    # treeview
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='landrover'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()
+
+
+
+def show_bmw():
+  
+
+    voitures = tk.Toplevel(cars)
+    voitures.geometry("300x300")
+    voitures.title('BMW')
+
+    # treeview
+    tree = ttk.Treeview(voitures, columns=("Marque", "Modèle", "Image","Type Carburant","Nombre de places","Transmission","Prix par jour","Disponibilité"))
+
+    tree.heading("#0", text="ID")
+    tree.heading("Marque", text="Marque")
+    tree.heading("Modèle", text="Modèle")
+    tree.heading("Image", text="Image")
+    tree.heading("Type Carburant", text="Type Carburant")
+    tree.heading("Nombre de places", text="Nombre de places")
+    tree.heading("Transmission", text="Transmission")
+    tree.heading("Prix par jour", text="Prix par jour")
+    tree.heading("Disponibilité", text="Disponibilité")
+
+
+
+    xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
+    tree.configure(xscrollcommand=xscrollbar.set)
+    xscrollbar.pack(side="bottom", fill="x")
+
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='bmw'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
+    voitures.geometry("500x500")
+
+
+    tree.pack()
+    tree.configure(height=24)
+
+      
+    voitures.mainloop()
+
+
+
+def show_mercedes():
   
 
     voitures = tk.Toplevel(cars)
@@ -34,28 +372,31 @@ def show_voitures():
     tree.heading("Prix par jour", text="Prix par jour")
     tree.heading("Disponibilité", text="Disponibilité")
 
-    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
-    mycursor=con.cursor()
-    query='SELECT * from mercedes'
-    mycursor.execute(query)
-    rows=mycursor.fetchall()
-    for row in rows:
-        tree.insert("",END,text=row[0],values=row[1:])
+
 
     xscrollbar = ttk.Scrollbar(voitures, orient="horizontal", command=tree.xview)
     tree.configure(xscrollcommand=xscrollbar.set)
     xscrollbar.pack(side="bottom", fill="x")
 
+
+
+    con= pymysql.connect(host='localhost',user='root',passwd='omarfettal10',database='cars')
+    mycursor=con.cursor()
+    query="SELECT * FROM voitures WHERE marque='mercedes'"
+    mycursor.execute(query)
+    resultat1=mycursor.fetchall()
+
+    for row in resultat1:
+        tree.insert("", "end",text=row[0],values=row[1:9]) 
+
+
     voitures.geometry("500x500")
 
 
-
-   
-
     tree.pack()
+    tree.configure(height=24)
 
       
-
     voitures.mainloop()
 
 
@@ -82,42 +423,42 @@ mercedesbutton.place(x=230,y=150)
 img1=Image.open('logobmw.jpg')
 resized_img1=img1.resize((120,120),Image.ANTIALIAS)
 img1_obj=ImageTk.PhotoImage(resized_img1)
-bmwbutton=Button(cars,image=img1_obj,command=show_bmw,cursor='hand2')
+bmwbutton=Button(cars,image=img1_obj,cursor='hand2',command=show_bmw)
 bmwbutton.place(x=450,y=150)
 
 #land rover button
 img2=Image.open('logorange.jpg')
 resized_img2=img2.resize((120,120),Image.ANTIALIAS)
 img2_obj=ImageTk.PhotoImage(resized_img2)
-rangebutton=Button(cars,image=img2_obj,command=show_landrover,cursor='hand2')
+rangebutton=Button(cars,image=img2_obj,cursor='hand2',command=show_landrover)
 rangebutton.place(x=670,y=150)
 
 #ford button
 img3=Image.open('logoford.jpg')
 resized_img3=img3.resize((120,120),Image.ANTIALIAS)
 img3_obj=ImageTk.PhotoImage(resized_img3)
-rangebutton=Button(cars,image=img3_obj,command=show_ford,cursor='hand2')
+rangebutton=Button(cars,image=img3_obj,cursor='hand2',command=show_ford)
 rangebutton.place(x=230,y=400)
 
 #renault button
 img4=Image.open('logorenault.jpg')
 resized_img4=img4.resize((120,120),Image.ANTIALIAS)
 img4_obj=ImageTk.PhotoImage(resized_img4)
-rangebutton=Button(cars,image=img4_obj,command=show_renault,cursor='hand2')
+rangebutton=Button(cars,image=img4_obj,cursor='hand2',command=show_renault)
 rangebutton.place(x=450,y=400)
 
 #dacia button
 img5=Image.open('logodacia.jpg')
 resized_img5=img5.resize((120,120),Image.ANTIALIAS)
 img5_obj=ImageTk.PhotoImage(resized_img5)
-rangebutton=Button(cars,image=img5_obj,command=show_dacia,cursor='hand2')
+rangebutton=Button(cars,image=img5_obj,cursor='hand2',command=show_dacia)
 rangebutton.place(x=670,y=400)
 
 #search button
 
 search_entry = tk.Entry(cars)
 search_entry.place(x=90,y=0)
-search_button = tk.Button(cars, text="Rechercher",command=search)
+search_button = tk.Button(cars, text="Rechercher",command=recherche)
 search_button.place(x=200,y=0)
 
 
